9770286bbcba1c587e9e4869d13b94b1cb144778
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-01-07 01:58:54 -0300

    second

--- src/App.java
+++ src/App.java
@@ -4,16 +4,30 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        int contador= 0;
+        int par= 0;
+        int impar = 0;
+        int positivo = 0;
+        int negativo = 0;
 
         for(int i = 0; i < 5; i++){
             int a = sc.nextInt();
             if(a%2 == 0){
-                contador++;
+                par++;
+            }else{
+                impar++;
             }
+            if(a > 0){
+                positivo++;
+            }else if(a < 0){
+                negativo++;
+            }
+
         }
 
         sc.close();
-        System.out.println(contador +" valores pares");
+        System.out.println(par +" valor(es) par(es)");
+        System.out.println(impar +" valor(es) impar(es)");
+        System.out.println(positivo +" valor(es) positivo(s)");
+        System.out.println(negativo +" valor(es) negativo(s)");
     }
 }
